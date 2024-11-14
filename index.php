6abad0a314fb54ea922088230cd36e023ddbdab0
wintopic <97203022+wintopic@users.noreply.github.com>
2024-11-14 16:10:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,31 @@
-<?php
+// api/random-pic.js
 
-//存有链接的文件名，这里是存放图片链接的txt文件
-$filename = "api.txt";
-if(!file_exists($filename)){
-die('文件不存在');
-}
+const fs = require('fs');
 
-//从文本获取链接
-$pics = [];
-$fs = fopen($filename, "r");
-while(!feof($fs)){
-$line=trim(fgets($fs));
-if($line!=''){
-array_push($pics, $line);
-}
-}
+module.exports = async (req, res) => {
+  // 定义存放图片链接的文件路径
+  const filePath = './img.txt';
+  
+  // 检查文件是否存在
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).json({ error: '文件不存在' });
+  }
 
-//从数组随机获取链接
-$pic = $pics[array_rand($pics)];
+  // 读取文件内容并按行分割
+  const fileContents = fs.readFileSync(filePath, 'utf-8');
+  const pics = fileContents.split('\n').map(line => line.trim()).filter(line => line !== '');
 
-//返回指定格式
-$type=$_GET['type'];
-switch($type){
+  // 随机选择一个图片链接
+  const pic = pics[Math.floor(Math.random() * pics.length)];
 
-//JSON返回
-case 'json':
-header('Content-type:text/json');
-die(json_encode(['pic'=>$pic]));
+  // 获取请求中的 type 参数
+  const type = req.query.type;
 
-default:
-die(header("Location: $pic"));
-}
+  // 根据请求的 type 返回不同的响应
+  if (type === 'json') {
+    return res.json({ pic });
+  }
+
+  // 默认情况：重定向到随机的图片
+  res.redirect(pic);
+};
